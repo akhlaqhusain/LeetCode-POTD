03c45a5ae9dca2adc64e0b8be73c6b00200ce14b
akhlaqhusain <akhlaqhusainoff@gmail.com>
2026-03-14 04:44:16 +0530

    update

---
+++ 3296.cpp
@@ -0,0 +1,40 @@
+/*
+    3296. Minimum Number of Seconds to Make Mountain Height Zero
+    Date: 13-03-2026
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        int maxWorkerTimes =
+            *max_element(workerTimes.begin(), workerTimes.end());
+        long long l = 1, r = static_cast<long long>(maxWorkerTimes) *
+                             mountainHeight * (mountainHeight + 1) / 2;
+        long long ans = 0;
+
+        while (l <= r) {
+            long long mid = (l + r) / 2;
+            long long cnt = 0;
+            for (int t : workerTimes) {
+                long long work = mid / t;
+                // find the largest k such that 1+2+...+k <= work
+                long long k = (-1.0 + sqrt(1 + work * 8)) / 2 + eps;
+                cnt += k;
+            }
+            if (cnt >= mountainHeight) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+private:
+    static constexpr double eps = 1e-7;
+};
